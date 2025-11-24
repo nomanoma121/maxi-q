32d6e366a00e5a99d73ed00fd5242b11fc2dd396
hinemosu1415 <167418833+hinemosu1415@users.noreply.github.com>
2025-11-24 12:59:03 +0900

    feat:/postQuesを分けた

--- packages/client/app/routes.ts
+++ packages/client/app/routes.ts
@@ -1,3 +1,7 @@
-import { index, type RouteConfig } from "@react-router/dev/routes";
+import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"),
+    route("/postQues", "routes/postQues.tsx"),
+] satisfies RouteConfig;
+
---
+++ packages/client/app/routes/postQues.tsx
@@ -0,0 +1,5 @@
+export default function PostQues() {
+    return (
+        <input type="text" placeholder="質問を入力してください" />
+    );
+}
